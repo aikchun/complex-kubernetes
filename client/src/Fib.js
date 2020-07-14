8e6e7fdf90cb1bab3defa82a23fcd792cfffaec9
AikChun <iam@simaikchun.com>
2020-07-14 10:40:17 +0800

    Changed the Client to troubleshoot the error.

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -39,6 +39,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
+    console.log("this.state.seenIndexes", this.state.seenIndexes);
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
