e70585ed552378b1cab3f0e18490aed37272d971
AikChun <iam@simaikchun.com>
2020-07-30 10:07:20 +0800

    Added deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t aikchun/multi-client -f ./client/Dockerfile ./client
-docker build -t aikchun/multi-server -f ./server/Dockerfile ./server
-docker build -t aikchun/multi-worker -f ./worker/Dockerfile ./worker
-docker push aikchun/multi-client
-docker push aikchun/multi-server
-docker push aikchun/multi-worker
+docker build -t aikchun/multi-client:latest -t aikchun/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aikchun/multi-server:latest -t aikchun/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aikchun/multi-worker:latest -t aikchun/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push aikchun/multi-client:latest
+docker push aikchun/multi-server:latest
+docker push aikchun/multi-worker:latest
+docker push aikchun/multi-client:$SHA
+docker push aikchun/multi-server:$SHA
+docker push aikchun/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=aikchun/multi-server
+kubectl set image deployments/server-deployment server=aikchun/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aikchun/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aikchun/multi-worker:$SHA
